f836863bb1c8ec62a3e8e3a4df272cf295092db7
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-23 09:53:39 +0200

    Add increasing views count

--- app/Controllers/NewsController.php
+++ app/Controllers/NewsController.php
@@ -25,19 +25,38 @@ class NewsController extends BasicCRUDController {
         JSONResponse::POSTResponse($result);
     }
 
+    public function getById($id) {
+        $record = $this->Model->getById($id);
+        JSONResponse::GETResponse($record);
+
+        $model = $this->increaseViewsCounter($record[0]);
+
+        $this->Model->updateById($id, $model);
+    }
+
     public function update($id) {
         $_PUT = $this->getPutData();
-
-        $model = [
-            'date' => $_PUT['date'],
-            'title' => $_PUT['title'],
-            'photo' => $_PUT['photo'],
-            'text' => $_PUT['text'],
-            'views' => $_PUT['views'],
-            'created_at' => date("Y-m-d H:i:s")
-        ];
+        
+        $model = $this->Model->getModel([
+                $_PUT['date'],
+                $_PUT['title'],
+                $_PUT['photo'],
+                $_PUT['text'],
+                $_PUT['views'],
+                date("Y-m-d H:i:s")
+        ]);
 
         $result = $this->Model->updateById($id, $model);
         JSONResponse::DefaultSuccessResponse($result);
+    }
+
+    private function increaseViewsCounter($record) {
+        $recordForModel = array_values($record);
+        array_shift($recordForModel);
+
+        $model = $this->Model->getModel($recordForModel);
+        $model['views'] += 1;
+
+        return $model;
     }
 }
