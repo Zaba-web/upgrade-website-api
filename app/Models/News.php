e98dda12181aa08d7afa3bf5e2f2e2a89be3ee20
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-23 07:13:36 +0200

    Add model

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,5 +5,13 @@ namespace App\Models;
 use App\Models\Core\Model;
 
 class News extends Model {
-    protected $tableName = "news";
+    protected $tableName = 'news';
+    protected $model = [
+        'date' => '',
+        'title' => '',
+        'photo' => '',
+        'text' => '',
+        'views' => 0,
+        'created_at' => null
+    ];
 }
