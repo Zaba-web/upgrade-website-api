2fe95f03114c62e9b3ed7bf8f575b1693653b582
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-23 07:14:32 +0200

    Add basic methods for working with database records

--- app/Models/Core/Model.php
+++ app/Models/Core/Model.php
@@ -6,6 +6,25 @@ use App\Database\DB;
 
 abstract class Model {
     protected $tableName = "";
+    protected $model = [];
+
+    /**
+     * Create model with data for new record
+     * 
+     * @param array $parameters values for new record
+     * 
+     * @return array model with values
+     */
+    public function getModel($parameters) {
+        $model = [];
+        $keys = array_keys($this->model);
+
+        for($i = 0; $i < count($keys); $i++) {
+            $model[$keys[$i]] = $parameters[$i];
+        }
+
+        return $model;
+    }
 
     /**
      * Get all records
@@ -24,7 +43,7 @@ abstract class Model {
     }
 
     /**
-     * Get one record by id
+     * Get single record by id
      * 
      * @return object result if something found
      * @return false if nothing was found
@@ -38,5 +57,97 @@ abstract class Model {
         }
 
         return false;
+    }
+
+    /**
+     * Create record
+     * 
+     * @param array $model model of the record
+     * 
+     * @return bool
+     */
+    public function create($model) {
+        $modelData = $this->handleInputModel($model);
+        $columnNames = $modelData[0];
+        $valuesToString = $modelData[1];
+
+        $queryString = "INSERT INTO " . $this->tableName . "(" . $columnNames . ") VALUES (" . $valuesToString . ")";
+
+        $queryResult = DB::GetInstance()->query($queryString);
+
+        if($queryResult) {
+            return true;
+        } 
+
+        return false;
+    }
+
+    /**
+     * Delete record by ID
+     * 
+     * @param int $id
+     * 
+     * @return bool
+     */
+    public function deleteById($_id) {
+        $id = DB::GetInstance()->real_escape_string($_id);
+
+        $queryResult = DB::GetInstance()->query("DELETE FROM " . $this->tableName . " WHERE id = '$id'");
+
+        if($queryResult) {
+            return true;
+        } 
+
+        return false;
+    }
+
+    public function updateById($_id, $model) {
+        $id = DB::GetInstance()->real_escape_string($_id);
+        $modelData = $this->handleInputModel($model, false);
+        $keys = $modelData[0];
+        $values = $modelData[1];
+
+        $updateArray = [];
+        
+        for($i = 0; $i < count($keys); $i++) {
+            $updateArray[] = $keys[$i] . "=" . $values[$i];
+        }
+
+        $resultString = implode(",", $updateArray);
+
+        $queryString = "UPDATE " . $this->tableName . " SET " . $resultString . " WHERE id = '$id'";
+
+        $queryResult = DB::GetInstance()->query($queryString);
+
+        if($queryResult) {
+            return true;
+        } 
+
+        return false;
+    }
+
+    /**
+     * Get data ready to insert into DB
+     * 
+     * @param array $model model of the record
+     */
+    private function handleInputModel($model, $implodeInString = true) {
+        $keys = array_keys($model);
+        $values = array_values($model);
+
+        for($i = 0; $i < count($values); $i++) {
+            $keys[$i] = DB::GetInstance()->real_escape_string($keys[$i]);
+            $values[$i] = DB::GetInstance()->real_escape_string($values[$i]);
+            $values[$i] = "'".$values[$i]."'";
+        }
+
+        if($implodeInString) {
+            $columnNames = implode(', ', $keys);
+            $valuesToString = implode(', ', $values);
+
+            return [$columnNames, $valuesToString];
+        } else {
+            return [$keys, $values];
+        }
     }
 }
