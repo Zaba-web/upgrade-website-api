948356ced1614f41be7b8be3c6171b3eeecf1da3
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-25 20:26:21 +0200

    Change name of called method

--- app/routes.php
+++ app/routes.php
@@ -2,26 +2,36 @@
 
 use App\Services\Routing\Route;
 use App\Controllers\NewsController;
+use App\Services\Validation\Rules\MinLength;
 
 $routes = [
-    Route::DECLARE('/news', function(){
+    Route::Declare('/news', function(){
         $newsController = new NewsController();
         $newsController->getAllRecords();
     }, "GET"),
-    Route::DECLARE('/news/*', function($id){
+
+    Route::Declare('/news/*', function($id){
         $newsController = new NewsController();
         $newsController->getById($id);
     }, "GET"),
-    Route::DECLARE('/news', function(){
+
+    Route::Declare('/news', function(){
         $newsController = new NewsController();
         $newsController->create();
     }, "POST"),
-    Route::DECLARE('/news/*', function($id){
+
+    Route::Declare('/news/*', function($id){
         $newsController = new NewsController();
         $newsController->delete($id);
     }, "DELETE"),
-    Route::DECLARE('/news/*', function($id){
+    
+    Route::Declare('/news/*', function($id){
         $newsController = new NewsController();
         $newsController->update($id);
-    }, "PUT")
+    }, "PUT"),
+
+    // Route::DECLARE('/test', function(){
+    //     $r = new MinLength("tes");
+    //     var_dump($r->validate(3));
+    // }, "GET")
 ];
