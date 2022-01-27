3461bf85c0d7e033129cd27f81f53a7697bc2ed2
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-28 00:58:20 +0200

    Add getUser method

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Database\DB;
 use App\Models\Core\Model;
 
 class User extends Model {
@@ -13,4 +14,21 @@ class User extends Model {
         'email' => '',
         'password' => ''
     ];
+
+    public function getUser($_email, $password) {
+        $email = DB::GetInstance()->real_escape_string($_email);
+        $queryResult = DB::GetInstance()->query("SELECT * FROM users WHERE email = '$email'");
+
+        if(!$queryResult) {
+            return false;
+        }
+
+        $user = DB::FetchResult($queryResult)[0];
+
+        if(password_verify($password, $user['password']) && $user['active'] != 0) {
+            return $user;
+        }
+
+        return false;
+    }
 }
