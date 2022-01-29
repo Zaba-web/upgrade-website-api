c525eda097ad89358afa9ed962579e81ae00ce81
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-29 20:55:23 +0200

    Add preflight code

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
+ use App\App;
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+} 
 
 require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__."/app/routes.php";
 
-use App\App;
-
 $app = new App();
 
 $app->setupRouter($routes);
+
