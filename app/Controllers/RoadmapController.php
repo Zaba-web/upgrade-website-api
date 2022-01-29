4be8d2933414d7d9fa5305d52793a79e605d9cf6
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-29 20:59:23 +0200

    Redefine getAllRecords method

--- app/Controllers/RoadmapController.php
+++ app/Controllers/RoadmapController.php
@@ -36,6 +36,11 @@ class RoadmapController extends BasicCRUDController {
         $model = $this->Model->getModel([$_PUT['item_status'], $_PUT['date_str'], $_PUT['title'], $_PUT['description'], $_PUT['image']]);
 
         $result = $this->Model->updateById($id, $model);
-        JSONResponse::POSTResponse($result);
+        JSONResponse::DefaultSuccessResponse($result);
+    }
+
+    public function getAllRecords() {
+        $allRecords = $this->Model->getAll("ASC");
+        JSONResponse::GETResponse($allRecords);
     }
 }
