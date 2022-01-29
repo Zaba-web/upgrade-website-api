b423d77d6f573b111673150a29fcc23e193bd856
Andriy Sotnyk <lodochnikkot@gmail.com>
2022-01-29 20:56:58 +0200

    Add getByType method

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Core\Model;
+use App\Database\DB;
 
 class Activity extends Model {
     protected $tableName = 'activities';
@@ -13,4 +14,14 @@ class Activity extends Model {
         'big_image' => '',
         'small_image' => ''
     ];
+
+    public function getByType($type) {
+        $queryResult = DB::GetInstance()->query("SELECT * FROM " . $this->tableName . " WHERE type = '$type' ORDER BY id  DESC");
+
+        if($queryResult) { 
+            return DB::FetchResult($queryResult);
+        }
+
+        return false;
+    }
 }
